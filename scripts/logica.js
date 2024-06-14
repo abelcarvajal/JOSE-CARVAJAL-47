86c7b2c78a4366a16e42d230c4c26a54a35b1cfe
ABEL CARVAJAL <joseabel.carvajal@gmail.com>
2024-06-14 11:29:27 -0500

    calculator interfaz and capture values

--- scripts/logica.js
+++ scripts/logica.js
@@ -1,1 +1,10 @@
-console.log('Jose Abel Carvajal Muñoz - 2721547')
+
+let valorPantalla = 0
+let aux = 0
+
+function boton(recibido){
+    console.log(recibido)
+    aux = document.getElementById("pantalla").value
+    document.getElementById("pantalla").value = aux + recibido
+    valorPantalla = document.getElementById("pantalla").value = aux + recibido
+}
